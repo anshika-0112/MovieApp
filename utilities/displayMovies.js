1b8521197d78760da4912c6cecdd42924891bb83
anshika-0112 <anshikabansal13@gmail.com>
2021-07-30 13:33:08 +0530

    Made some changes

--- utilities/displayMovies.js
+++ utilities/displayMovies.js
@@ -3,6 +3,6 @@ export const displayMovieInfo = (movieInfo) => {
   const movieList = document.getElementById("movieList");
   movieList.innerHTML = "";
   movieInfo.forEach((movie) => {
-    movieList.appendChild(createMovieInfo(movie));
+    if (movie.Poster !== "N/A") movieList.appendChild(createMovieInfo(movie));
   });
 };
--- utilities/getMoviesInfo.js
+++ utilities/getMoviesInfo.js
@@ -1,17 +1,24 @@
 import { displayMovieInfo } from "./displayMovies.js";
-export const getMovieInfo = () => {
+export const getMovieInfo = async () => {
   const searchMovie = document.querySelector("input");
+  const noMovieFound = document.getElementById("movieNotFound");
+  const movieList = document.getElementById("movieList");
   const apiKey = "8af58d6d";
   const url =
     "https://www.omdbapi.com/?i=tt3896198&apikey=" +
     apiKey +
     "&s=" +
     searchMovie.value;
-  fetch(url)
-    .then((response) => response.json())
-    .catch((error) => console.log(error))
-    .then((response) => {
-      displayMovieInfo(response.Search);
-    })
-    .catch((error) => console.log("catch" + error));
+  const movieResponse = await fetch(url).catch((error) => console.log(error));
+  if (movieResponse.status >= 200 && movieResponse.status <= 299) {
+    const movieInfo = await movieResponse.json();
+    if (movieInfo.Response !== "False") {
+      noMovieFound.style.display = "none";
+      displayMovieInfo(movieInfo.Search);
+    } else {
+      movieList.innerHTML = "";
+      noMovieFound.style.display = "block";
+    }
+    console.log(movieInfo);
+  }
 };
